0ac878c85bbb01ddd335f04cf34057830f270ac1
peter <lih@lafayette.edu>
2020-04-26 14:50:29 -0400

    change footer; revise tag selection

--- src/app/helper/capitalize-first-letter.ts
+++ src/app/helper/capitalize-first-letter.ts
@@ -1,3 +1,3 @@
 export function capitalizeFirstLetter(str: string): string {
-  return str.charAt(0).toUpperCase().concat(str.substring(1));
+  return str?.charAt(0).toUpperCase().concat(str.substring(1));
 }
